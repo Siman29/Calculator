98c0b613b3f64fad92c52aefa57d30f7bcbd0426
Siman29 <60322023+Siman29@users.noreply.github.com>
2020-05-29 15:28:58 +0530

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -11,21 +11,25 @@ function calc1(clickedCellEvent) {
     var clickcell = clickedCellEvent.target;
     var clickedcellindex = parseInt(clickcell.getAttribute('id'));
     var input = "";
-    if (clickedcellindex === 10) {
+    if (clickedcellindex === 11) {
         input = "*";
-    } else if (clickedcellindex === 11) {
-        input = "/";
     } else if (clickedcellindex === 12) {
-        input = "-";
+        input = "/";
     } else if (clickedcellindex === 13) {
+        input = "-";
+    } else if (clickedcellindex === 14) {
         input = "+";
+    } else if (clickedcellindex === 15) {
+        input = "^";
+    } else if (clickedcellindex === 16) {
+        input = "r";
     }
     var check = "";
     var prev_input = "";
     var display = document.getElementById("prob");
     prev_input = display.value;
     check = prev_input[prev_input.length - 1];
-    if (display.value.trim() !== "" && (check !== "*") && (check !== "/") && (check !== "-") && (check !== "+")) {
+    if (display.value.trim() !== "" && (check !== "*") && (check !== "/") && (check !== "-") && (check !== "+") && (check !== "^") && (check !== "r")) {
         var newvalue = display.value + input;
         display.value = newvalue;
     } else {
@@ -44,7 +48,7 @@ function soln() {
     check = prob[prob.length - 1];
     var ele = "";
     var c = 1;
-    if (input.value.trim() !== "" && (check !== "*") && (check !== "/") && (check !== "-") && (check !== "+")) {
+    if (input.value.trim() !== "" && (check !== "*") && (check !== "/") && (check !== "-") && (check !== "+") && (check !== "^") && (check !== "r")) {
         for (var i = 0; i < prob.length; i = i + c) {
             if (prob[i] === "0" || prob[i] === "1" || prob[i] === "2" || prob[i] === "3" || prob[i] === "4" || prob[i] === "5" || prob[i] === "6" || prob[i] === "7" || prob[i] === "8" || prob[i] === "9") {
                 ele = prob[i];
@@ -64,27 +68,39 @@ function soln() {
 
         }
         for (var i = 0; i < elements.length; i++) {
+            if (elements[i] === "^") {
+                var k = parseInt(elements[i - 1]) ** parseInt(elements[i + 1]);
+                elements[i] = k;
+                elements.splice(i + 1, 1);
+                elements.splice(i - 1, 1);
+            }
+            if (elements[i] === "r") {
+                var q = parseInt(elements[i - 1]) % parseInt(elements[i + 1]);
+                elements[i] = q;
+                elements.splice(i + 1, 1);
+                elements.splice(i - 1, 1);
+            }
             if (elements[i] === "/") {
                 var a = parseInt(elements[i - 1]) / parseInt(elements[i + 1]);
-                elements[i] = a
+                elements[i] = a;
                 elements.splice(i + 1, 1);
                 elements.splice(i - 1, 1);
             }
             if (elements[i] === "*") {
                 var b = parseInt(elements[i - 1]) * parseInt(elements[i + 1]);
-                elements[i] = b
+                elements[i] = b;
                 elements.splice(i + 1, 1);
                 elements.splice(i - 1, 1);
             }
             if (elements[i] === "+") {
                 var c = parseInt(elements[i - 1]) + parseInt(elements[i + 1]);
-                elements[i] = c
+                elements[i] = c;
                 elements.splice(i + 1, 1);
                 elements.splice(i - 1, 1);
             }
             if (elements[i] === "-") {
                 var d = parseInt(elements[i - 1]) - parseInt(elements[i + 1]);
-                elements[i] = d
+                elements[i] = d;
                 elements.splice(i + 1, 1);
                 elements.splice(i - 1, 1);
             }
@@ -110,6 +126,16 @@ function soln() {
                     elements[0] = e;
                     elements.pop();
                     elements.pop();
+                } else if (s === "^") {
+                    var e = parseInt(elements[0]) ** parseInt(elements[2]);
+                    elements[0] = e;
+                    elements.pop();
+                    elements.pop();
+                } else if (s === "r") {
+                    var e = parseInt(elements[0]) % parseInt(elements[2]);
+                    elements[0] = e;
+                    elements.pop();
+                    elements.pop();
                 }
             }
         }
@@ -119,6 +145,27 @@ function soln() {
     }
 }
 
+function delete_recent() {
+    var input = document.getElementById("prob");
+    var elements = ["1"];
+    elements = [];
+    prob = input.value;
+    if (input.value.trim() !== "") {
+        for (var i = 0; i < prob.length; i++) {
+            elements.push(prob[i]);
+        }
+        elements.pop();
+        var m = "";
+        for (var i = 0; i < elements.length; i++) {
+            m = m + elements[i];
+        }
+        input.value = m;
+    } else {
+        alert("No Blanks are allowed");
+    }
+}
+
 document.querySelectorAll('.button1').forEach(button1 => button1.addEventListener('click', calc));
 document.querySelectorAll('.sign').forEach(sign => sign.addEventListener('click', calc1));
 document.querySelector('.solution').addEventListener('click', soln);
+document.querySelector('.delete').addEventListener('click', delete_recent);
